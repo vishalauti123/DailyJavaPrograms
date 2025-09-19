df5aea5fe7d57913a9354f9b5195b9361edc55d3
Vishal Auti <vishalauti45@gmail.com>
2025-09-19 19:08:38 +0530

    Largest three Number

--- LargestOfThree.java
+++ LargestOfThree.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class LargestOfThree {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter three numbers: ");
+        System.out.print("Enter 3 numbers: ");
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
